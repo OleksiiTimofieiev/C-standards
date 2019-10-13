0ae0e0003eecb58bca5b38e3a1ef2bdec100c2e3
contrbas@gmail.com <K4rmU78B>
2019-10-13 11:32:50 +0300

    starting to work on 11 standard

---
+++ 11_14/samples.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/*
+  	1. nullptr solves:
+	void	f(void *)
+	void	f(int x)
+
+	f(nullptr)
+	std::nullptr_t can be casted to any pointer;
+
+	2. enum class:
+	enum class Enum1 {a,b,c}
+	need to use :: semantics
+
+	3. explicit operator:
+	only implicit conversion
+
+	4. typedef:
+	For template classes.
+
+*/
+
+int		main(void)
+{
+
+
+	return (0);
+}
